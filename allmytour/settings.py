ebfeedb5df2df55603e6f7b7a3af16e75bbf9135
김훈태 <hoontae.kim@Hoontaes-MacBookPro.local>
2021-08-30 12:06:58 +0900

    Add: project setting1

--- allmytour/settings.py
+++ allmytour/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^cnq=9bi^aaf=3va=pum2sa@*oj3ek9n^ql1277rn91&9q+9yt'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,12 +75,7 @@ WSGI_APPLICATION = 'allmytour.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASES
 
 
 # Password validation
