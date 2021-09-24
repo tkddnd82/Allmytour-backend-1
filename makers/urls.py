34ec1ae9a4dee309eee3d8ea78a3a0ef2030e54f
gnsxo9 <gnsxo9@gmail.com>
2021-09-24 10:19:27 +0000

    [wecode 김훈태] makers 등록

--- makers/urls.py
+++ makers/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
-from makers.views import MakerView
+from makers.views import MakerView #OptionView 
 
 urlpatterns = [
     path('', MakerView.as_view()),
     path('/<int:maker_id>',MakerView.as_view()),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    #path('/option',OptionView.as_view()),
+] 
